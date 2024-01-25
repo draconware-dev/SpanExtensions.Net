f8d5f54a9101096fbabe110acbd0e8eddcb41ed7
dragon <tim.clemens0703@gmail.com>
2024-01-25 15:26:03 +0100

    Revert "quick."
    This reverts commit 956bdd39f76b9716408ae243db1730090b47eb70.

--- src/Extensions/Span/SpanExtensions.Linq.cs
+++ src/Extensions/Span/SpanExtensions.Linq.cs
@@ -222,52 +222,6 @@ namespace SpanExtensions
         }
 
         /// <summary> 
-        /// Bypasses elements in <paramref name="source"/> as long as a <paramref name="condition"/> is true and then returns the remaining elements. The element's index is used in the logic of the predicate function.
-        /// </summary>
-        /// <typeparam name="T">The type of elements in the <see cref="ReadOnlySpan{T}"/>.</typeparam>
-        /// <param name="source">The <see cref="ReadOnlySpan{T}"/> to operate on.</param>
-        /// <param name="condition">A function to test each element for a condition.</param> 
-        /// <returns>A <see cref="ReadOnlySpan{T}"/> that contains the elements from <paramref name="source"/> starting at the first element in the linear series that does not pass the specified <paramref name="condition" />.</returns>
-        /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="condition"/> is null.</exception>
-        public static Span<T> SkipWhile<T>(this Span<T> source, Predicate<T> condition)
-        {
-            int count = 0;
-            while(count < source.Length)
-            {
-                T t = source[count];
-                if(!condition(t))
-                {
-                    return source.Skip(count);
-                }
-                count++;
-            }
-            return Span<T>.Empty;
-        }
-
-        ///  <summary> 
-        /// Returns elements from <paramref name="source"/> as long as a specified <paramref name="condition"/> is true, and then skips the remaining elements.   
-        /// </summary> 
-        /// <typeparam name="T">The type of elements in the <see cref="ReadOnlySpan{T}"/>.</typeparam> 
-        /// <param name="source">The <see cref="ReadOnlySpan{T}"/> to operate on.</param>  
-        /// <param name="condition">A function to test each element for a condition.</param>    
-        /// <returns>A <see cref="ReadOnlySpan{T}"/> that contains elements from <paramref name="source"/> that occur before the element at which the <paramref name="condition"/> no longer passes.</returns> 
-        /// <exception cref="ArgumentNullException"><paramref name="source"/> or <paramref name="condition"/> is null.</exception>
-        public static Span<T> TakeWhile<T>(this Span<T> source, Predicate<T> condition)
-        {
-            int count = 0;
-            while(count < source.Length)
-            {
-                T t = source[count];
-                if(!condition(t))
-                {
-                    return source.Take(count);
-                }
-                count++;
-            }
-            return Span<T>.Empty;
-        }
-        
-        /// <summary> 
         /// Returns a new <see cref="Span{T}"/> that contains the elements from source with the last <paramref name="count"/> elements of the source collection omitted.
         /// </summary>
         /// <typeparam name="T">The type of elements in the <see cref="Span{T}"/>.</typeparam>
