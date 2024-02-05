f2464ae7c57c81eb3805a9b05498bb9530850051
dragon <tim.clemens0703@gmail.com>
2024-02-05 17:27:17 +0100

    added benchmarking

---
+++ PerformanceTests/ReadOnlySpan_Split_Any_Benchmark.cs
@@ -0,0 +1,36 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SpanExtensions.Tests.Performance
+{
+    [MemoryDiagnoser(false)]
+    public class ReadOnlySpan_Split_Any_Benchmark
+    {
+
+        [Benchmark]
+        [ArgumentsSource(nameof(GetArgsWithDelimiter))]
+        public int Split_Any_ReadOnlySpan(ReadOnlySpan<char> value, char[] delimiters)
+        {
+            int count = 0;
+            foreach(ReadOnlySpan<char> part in value.SplitAny(delimiters))
+            {
+                count += part.Length;
+            }
+            return count;
+        }
+
+        [Benchmark(Baseline = true)]
+        [ArgumentsSource(nameof(GetArgsWithDelimiter))]
+        public int Split_Any_String(string value, char[] delimiters)
+        {
+            return value.Split(delimiters).Length;
+        }
+
+        public IEnumerable<object[]> GetArgsWithDelimiter()
+        {
+            yield return new object[] { "abcde", new object[] { 'b', 'd' } }; 
+            yield return new object[] { "abcdefg", new object[] { 'b', 'd', 'f' } };
+            yield return new object[] { "abba", new object[] { 'b', 'a' } };
+            yield return new object[] { "1234567890", new object[] { '1', '5', '7' } };
+        }
+    }
+}
---
+++ PerformanceTests/ReadOnlySpan_Split_Benchmark.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SpanExtensions.Tests.Performance
+{
+    [MemoryDiagnoser(false)]
+    public class ReadOnlySpan_Split_Benchmark
+    {
+
+        [Benchmark]
+        [ArgumentsSource(nameof(GetArgsWithDelimiter))]
+        public int Split_ReadOnlySpan(ReadOnlySpan<char> value, char delimiter)
+        {
+            int count = 0;
+            foreach(ReadOnlySpan<char> part in value.Split(delimiter))
+            {
+                count += part.Length;
+            }
+            return count;
+        }
+
+        [Benchmark(Baseline = true)]
+        [ArgumentsSource(nameof(GetArgsWithDelimiter))]
+        public int Split_String(string value, char delimiter)
+        {
+            return value.Split(delimiter).Length;
+        }
+
+        public IEnumerable<object[]> GetArgsWithDelimiter()
+        {
+            yield return new object[] { "xyz", 'y' };
+            yield return new object[] { "abcde", 'c' };
+            yield return new object[] { "abcdefg", 'd' };
+            yield return new object[] { "abba", 'b' };
+            yield return new object[] { "aaa", 'a' };
+            yield return new object[] { "aba", 'a' };
+            yield return new object[] { "12131415161718190", '1' };
+        }
+    } 
+}
