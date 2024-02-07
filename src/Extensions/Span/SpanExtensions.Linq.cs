cd4efa1cb0f05c1cec1e79328875524fc35ed9bb
SadToBeNep <egocentricnep@gmail.com>
2024-02-07 15:55:08 +0100

    Min function for Span<>, forgot to push after implementing it for ReadOnlySpan<>

--- src/Extensions/Span/SpanExtensions.Linq.cs
+++ src/Extensions/Span/SpanExtensions.Linq.cs
@@ -460,6 +460,166 @@ namespace SpanExtensions
             return ReadOnlySpanExtensions.Average(source);
         }
 #endif
+/*#################################### MIN ##################################*/
+#if NET7_0_OR_GREATER
 
+        /// <summary> 
+        /// Computes the Min of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <typeparam name="T">The type of elements in <paramref name="source"/>.</typeparam> 
+        /// <param name="source">The <see cref="Span{T}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static T Min<T>(this Span<T> source) where T : INumber<T>
+        {
+            T sum = source.Sum();
+            return sum / T.CreateChecked(source.Length);
+        }
+#else
+
+#if NET5_0_OR_GREATER
+
+        /// <summary> 
+        /// Computes the Average of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <param name="source">The <see cref="Span{Half}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static Half Min(this Span<Half> source)
+        {
+            return ReadOnlySpanExtensions.Min(source);
+        }
+#endif
+        /// <summary> 
+        /// Computes the Min of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <param name="source">The <see cref="Span{Byte}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static byte Min(this Span<byte> source)
+        {
+            return ReadOnlySpanExtensions.Min(source);
+        }
+
+        /// <summary> 
+        /// Computes the Min of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <param name="source">The <see cref="Span{UInt16}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static ushort Min(this Span<ushort> source)
+        {
+            return ReadOnlySpanExtensions.Min(source);
+        }
+
+        /// <summary> 
+        /// Computes the Min of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <param name="source">The <see cref="Span{uint32}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static uint Min(this Span<uint> source)
+        {
+            return ReadOnlySpanExtensions.Min(source);
+        }
+        /// <summary> 
+        /// Computes the Min of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <param name="source">The <see cref="Span{UInt64}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static ulong Min(this Span<ulong> source)
+        {
+            return ReadOnlySpanExtensions.Min(source);
+        }
+
+        /// <summary> 
+        /// Computes the Min of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <param name="source">The <see cref="Span{SByte}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static sbyte Min(this Span<sbyte> source)
+        {
+            return ReadOnlySpanExtensions.Min(source);
+        }
+
+        /// <summary> 
+        /// Computes the Min of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <param name="source">The <see cref="Span{Int16}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static short Min(this Span<short> source)
+        {
+            return ReadOnlySpanExtensions.Min(source);
+        }
+
+        /// <summary> 
+        /// Computes the Min of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <param name="source">The <see cref="Span{Int32}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static int Min(this Span<int> source)
+        {
+            return ReadOnlySpanExtensions.Min(source);
+        }
+
+        /// <summary> 
+        /// Computes the Min of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <param name="source">The <see cref="Span{Int64}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static long Min(this Span<long> source)
+        {
+            return ReadOnlySpanExtensions.Min(source);
+        }
+
+        /// <summary> 
+        /// Computes the Min of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <param name="source">The <see cref="Span{Single}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static float Min(this Span<float> source)
+        {
+            return ReadOnlySpanExtensions.Min(source);
+        }
+
+        /// <summary> 
+        /// Computes the Min of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <param name="source">The <see cref="Span{Double}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static double Min(this Span<double> source)
+        {
+            return ReadOnlySpanExtensions.Min(source);
+        }
+
+        /// <summary> 
+        /// Computes the Min of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <param name="source">The <see cref="Span{Decimal}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static decimal Min(this Span<decimal> source)
+        {
+            return ReadOnlySpanExtensions.Min(source);
+        }
+
+        /// <summary> 
+        /// Computes the Min of all the values in <paramref name="source"/>. 
+        /// </summary>  
+        /// <param name="source">The <see cref="Span{BigInteger}"/> to operate on.</param>    
+        /// <returns>The Min out of all the values in <paramref name="source"/>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/> is null.</exception>
+        public static BigInteger Min(this Span<BigInteger> source)
+        {
+            return ReadOnlySpanExtensions.Min(source);
+        }
+#endif
     }
 }
