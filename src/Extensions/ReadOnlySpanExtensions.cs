51ec89fc499c7704051b5321743081a72cdb1f6c
dragon <tim.clemens0703@gmail.com>
2024-02-19 20:35:31 +0100

    docs..

---
+++ src/Extensions/ReadOnlySpanExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SpanExtensions
+{
+    /// <summary>
+    /// Extension Methods for <see cref="ReadOnlySpan{T}"/>.
+    /// </summary>
+    public static partial class ReadOnlySpanExtensions
+    {
+    }
+}
