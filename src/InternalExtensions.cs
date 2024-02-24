0de00adc04aa4c0c19d0ecdeb346e4d67ceb19fa
Guiorgy <Guiorgy123@gmail.com>
2024-02-24 19:02:36 +0400

    Silence warnings
    f

--- src/InternalExtensions.cs
+++ src/InternalExtensions.cs
@@ -1,38 +1,50 @@
-﻿using System;
+using System;
+#if NETCOREAPP3_0_OR_GREATER
 using System.Runtime.CompilerServices;
+#endif
 
 namespace SpanExtensions
 {
     static class InternalExtensions
     {
+#if NETCOREAPP3_0_OR_GREATER
         /// <summary>
         /// Throws an <see cref="ArgumentOutOfRangeException"/> if <paramref name="value"/> is negative.
         /// </summary>
         /// <param name="value">The argument to validate as non-negative.</param>
         /// <param name="paramName">The name of the parameter with which <paramref name="value"/> corresponds.</param>
         /// <returns><paramref name="value"/>.</returns>
-        public static int ThrowIfNegative(this int value
-#if NETCOREAPP3_0_OR_GREATER
-            , [CallerArgumentExpression(nameof(value))] string? paramName = null
-#endif
-            )
+        public static int ThrowIfNegative(this int value, [CallerArgumentExpression(nameof(value))] string? paramName = null)
         {
 #if NET8_0_OR_GREATER
             ArgumentOutOfRangeException.ThrowIfNegative(value, paramName);
-#elif NETCOREAPP3_0_OR_GREATER
-            if(value < 0)
-            {
-                throw new ArgumentOutOfRangeException(paramName, value, $"{paramName} ('{value}') must be a non-negative value.");
-            }
 #else
             if(value < 0)
             {
-                throw new ArgumentOutOfRangeException(null, value, $" ('{value}') must be a non-negative value.");
+                throw new ArgumentOutOfRangeException(paramName, value, $"{paramName} ('{value}') must be a non-negative value.");
             }
 #endif
 
             return value;
         }
+#else
+        /// <summary>
+        /// Throws an <see cref="ArgumentOutOfRangeException"/> if <paramref name="value"/> is negative.
+        /// </summary>
+        /// <param name="value">The argument to validate as non-negative.</param>
+        /// <returns><paramref name="value"/>.</returns>
+        public static int ThrowIfNegative(this int value)
+        {
+            if(value < 0)
+            {
+#pragma warning disable S3928 // Parameter names used into ArgumentException constructors should match an existing one
+                throw new ArgumentOutOfRangeException(null, value, $" ('{value}') must be a non-negative value.");
+#pragma warning restore S3928 // Parameter names used into ArgumentException constructors should match an existing one
+            }
+
+            return value;
+        }
+#endif
 
         /// <summary>
         /// Determines whether the <see cref="StringSplitOptions.RemoveEmptyEntries"/> bit field is set in the current instance.
@@ -44,6 +56,7 @@ namespace SpanExtensions
             return options.HasFlag(StringSplitOptions.RemoveEmptyEntries);
         }
 
+#if NET5_0_OR_GREATER
         /// <summary>
         /// Determines whether the <see cref="StringSplitOptions.TrimEntries"/> bit field is set in the current instance.
         /// </summary>
@@ -54,11 +67,18 @@ namespace SpanExtensions
         /// <returns><see langword="true"/> if <paramref name="options"/> has the <see cref="StringSplitOptions.TrimEntries"/> bit field set; <see langword="false"/> otherwise.</returns>
         public static bool IsTrimEntriesSet(this StringSplitOptions options)
         {
-#if NET5_0_OR_GREATER
             return options.HasFlag(StringSplitOptions.TrimEntries);
+    }
 #else
+        /// <summary>
+        /// Always returns false.
+        /// </summary>
+        /// <param name="options">The <see cref="StringSplitOptions"/> instance to test.</param>
+        /// <returns><see langword="false"/>.</returns>
+        public static bool IsTrimEntriesSet(this StringSplitOptions options)
+        {
             return false;
-#endif
         }
+#endif
     }
 }
