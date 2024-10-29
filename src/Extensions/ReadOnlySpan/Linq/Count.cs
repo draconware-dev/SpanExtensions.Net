3933e24a9bc317e56655ad33c492683294dde255
dragon7307 <tim.clemens0703@gmail.com>
2024-10-29 11:32:01 +0100

    added Count overloads for pre net8.0 versions.

--- src/Extensions/ReadOnlySpan/Linq/Count.cs
+++ src/Extensions/ReadOnlySpan/Linq/Count.cs
@@ -47,5 +47,48 @@ namespace SpanExtensions
 
             return count;
         }
+
+#if !NET8_0_OR_GREATER
+        /// <summary>Counts the number of times the specified <paramref name="value"/> occurs in the <paramref name="source"/>.</summary>
+        /// <typeparam name="T">The element type of the span.</typeparam>
+        /// <param name="source">A <see cref="ReadOnlySpan{T}"/> whose elements are to be counted.</param>
+        /// <param name="value">The value for which to search.</param>
+        /// <returns>The number of elements eqaul to <paramref name="value"/> in <paramref name="source"/>.</returns>
+        /// <exception cref="OverflowException">The number of elements in <paramref name="source"/> is larger than <see cref="int.MaxValue"/>.</exception>
+        public static int Count<T>(this ReadOnlySpan<T> source, T value) where T : IEquatable<T>
+        {
+            int count = 0;
+
+            foreach(var item in source)
+            {
+                if(item.Equals(value))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        /// <summary>Counts the number of times the specified <paramref name="value"/> occurs in the <paramref name="source"/>.</summary>
+        /// <typeparam name="T">The element type of the span.</typeparam>
+        /// <param name="source">A <see cref="ReadOnlySpan{T}"/> whose elements are to be counted.</param>
+        /// <param name="value">The value for which to search.</param>
+        /// <returns>The number of elements eqaul to <paramref name="value"/> in <paramref name="source"/>.</returns>
+        /// <exception cref="OverflowException">The number of elements in <paramref name="source"/> is larger than <see cref="int.MaxValue"/>.</exception>
+        public static int Count<T>(this ReadOnlySpan<T> source, ReadOnlySpan<T> value) where T : IEquatable<T>
+        {
+            int count = 0;
+            int current = 0;
+
+            while((current = source.IndexOf(value)) != -1)
+            {
+                source = source.Slice(current + value.Length);
+                count++;
+            }
+
+            return count;
+        }
+#endif
     }
 }
